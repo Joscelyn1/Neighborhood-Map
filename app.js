bdaf5bdd7fe9cdbe8b5ecd1c3e0cd5f7a94a22c6
Joscelyn <JoscelynRose@gmail.com>
2018-06-01 18:18:19 -0700

    Made massive changes, based my code on example code at https://github.com/udacity/ud864

--- app.js
+++ app.js
@@ -1,69 +1,102 @@
 
-
-var markers = [
-                    {
-                        name : 'Ronald Reagan Library',
-                        position : {lat: 34.2598767, lng: 118.81982089999997},
-                        lat : 34.2598767,
-                        long : 118.81982089999997,
-                    },
-                    {
-                        name : 'Simi Valley Library',
-                        position : {lat: 34.28901761295414, lng: -118.7193630981476},
-                        lat : 34.28901761295414,
-                        long : -118.7193630981476,
-                    },
-                    {
-                        name : 'Campos',
-                        position : {lat: 34.2744238, lng: -118.70930759999999},
-                        lat : 34.2744238,
-                        long : -118.70930759999999,
-                    },
-                    {
-                        name : 'Lemon Park',
-                        position : {lat: 34.2896751, lng: -118.7239667},
-                        lat : 34.2896751,
-                        long : -118.7239667,
-                    },
-                    {
-                        name : 'Arroyo Simi Bike Path',
-                        position : {lat: 34.268684, lng: -118.705252},
-                        lat : 34.268684,
-                        long : -118.705252
-                    }
-];
+/* ======= model ======= */
 
 
+var model = {
+  locations: [
+    {title: 'Ronald Reagan Library', location: {lat: 34.2597858, lng: -118.81989599999997}},
+    {title: 'Simi Valley Library', location: {lat: 34.28901761295414, lng: -118.7193630981476}},
+    {title: 'Campos', location: {lat: 34.2744238, lng: -118.70930759999999}},
+    {title: 'Lemon Park', location: {lat: 34.2896751, lng: -118.7239667}},
+    {title: 'Arroyo Simi Bike Path', location: {lat: 34.268684, lng: -118.705252}}
+  ]
 
-var Marker = function(data) {
-    this.name = ko.observable(data.name);
-    this.lat = ko.observable(data.lat);
-    this.long = ko.observable(data.long);
-    this.position = ko.observable(data.position);
-    this.marker = new google.maps.Marker({
-        position: data.position,
-        setMap: map,
-        title: data.name
-      });
+};
 
-}
+/* ======= Google API ======= */
+var map;
+
+// Create a new blank array for all the listing markers.
+var markers = [];
+
+function initMap() {
+  var simiValley = {lat: 34.269447, lng: -118.781479};
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: simiValley,
+    zoom: 13,
+    mapTypeControl: false
+  });
 
 
+  var largeInfowindow = new google.maps.InfoWindow();
 
-var viewModel = function() {
-    var self = this;
-// make an empty array called "markerList"
-    this.markerList = ko.observableArray([]);
-// push each marker into the markerList array
-    markers.forEach(function(markerItem) {
-        self.markerList.push( new Marker(markerItem) );
+  // The following group uses the location array to create an array of markers on initialize.
+  for (var i = 0; i < model.locations.length; i++) {
+    // Get the position from the location array.
+    var position = model.locations[i].location;
+    var title = model.locations[i].title;
+    // Create a marker per location, and put into markers array.
+     var marker = new google.maps.Marker({
+      position: position,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
     });
-
-
-    this.currentMarker = ko.observable( this.markerList()[0] );
-
-
+    // Push the marker to our array of markers.
+    markers.push(marker);
+    // Create an onclick event to open an infowindow at each marker.
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+  }
+  document.getElementById('show-listings').addEventListener('click', showListings);
+  document.getElementById('hide-listings').addEventListener('click', hideListings);
 }
 
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+  if (infowindow.marker != marker) {
+    infowindow.marker = marker;
+    infowindow.setContent('<div>' + marker.title + '</div>');
+    infowindow.open(map, marker);
+    // Make sure the marker property is cleared if the infowindow is closed.
+    infowindow.addListener('closeclick', function() {
+      infowindow.marker = null;
+    });
+  }
+}
+
+// This function will loop through the markers array and display them all.
+function showListings() {
+  var bounds = new google.maps.LatLngBounds();
+  // Extend the boundaries of the map for each marker and display the marker
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+    bounds.extend(markers[i].position);
+  }
+  map.fitBounds(bounds);
+}
+
+// This function will loop through the listings and hide them all.
+function hideListings() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+}
+
+/* ======= viewModel ======= */
+
+var viewModel = function(model) {
+  var self = this;
+  self.markerList = ko.observableArray(model.locations);
+
+}
 
 ko.applyBindings(new viewModel());
+
+
+
